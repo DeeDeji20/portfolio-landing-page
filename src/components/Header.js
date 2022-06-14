c61176678e728dbdd84f8a33cb18df69f3665890
Adeola Oladeji <deolaoladeji@gmail.com>
2022-06-14 20:37:32 +0100

    updates

--- src/components/Header.js
+++ src/components/Header.js
@@ -12,11 +12,11 @@ const Header = () => {
     <Stack >
         <Circle position="absolute" bg="blue" opacity="0.1" w="300px" h="300px" alignSelf="flex-end" display={{base:"none", md:"block", lg:"block"}} />
         <Flex direction={isNotSmallScreen ? "row":"column"}
-            p={isNotSmallScreen ? "32" : "0"}
+            p={isNotSmallScreen ? "32" : "10"}
             alignSelf="center">
             
             <Box
-             mt={isNotSmallScreen ? "0":"290px"} align ="flex-start" textAlign={{base:"center", md:"left", lg:"left"}}  alignItems={{base:"center", md:"center", lg:"flex-start"}} w="100">
+             mt={isNotSmallScreen ? "0":"200px"} align ="flex-start" textAlign={{base:"center", md:"left", lg:"left"}}  alignItems={{base:"center", md:"center", lg:"flex-start"}} w="100">
                 <Text fontSize={{base:"3xl", md:"5xl", lg:"5xl"}} fontWeight="semibold">Hi, I am</Text>   
                 <Text fontSize={{base:"5xl", md:"5xl", lg:"7xl"}} fontWeight="bold" bgGradient="linear(to-r, cyan.400, blue.200, purple.600)" bgClip="text" textAlign={{base:"center", md:"left"}} w="inherit">Adeola Oladeji</Text> 
                 <Spacer/>
